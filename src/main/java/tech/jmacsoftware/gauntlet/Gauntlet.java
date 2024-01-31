c308dbfcf1834a08e81541139dedf83252fde14c
jaymac00 <jmm0191@auburn.edu>
2024-01-31 11:48:58 -0600

    register head events in plugin

--- src/main/java/tech/jmacsoftware/gauntlet/Gauntlet.java
+++ src/main/java/tech/jmacsoftware/gauntlet/Gauntlet.java
@@ -1,26 +1,19 @@
 package tech.jmacsoftware.gauntlet;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bukkit.ChatColor;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import tech.jmacsoftware.gauntlet.events.GraveEvents;
+import tech.jmacsoftware.gauntlet.events.HeadEvents;
 import tech.jmacsoftware.gauntlet.events.TunnelingEvents;
 import tech.jmacsoftware.gauntlet.items.ToolRecipes;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Arrays;
-
 public class Gauntlet extends JavaPlugin {
 
-	private final ObjectMapper object_mapper = new ObjectMapper();
-
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(new TunnelingEvents(), this);
 		getServer().getPluginManager().registerEvents(new GraveEvents(), this);
+		getServer().getPluginManager().registerEvents(new HeadEvents(), this);
+		getServer().getPluginManager().registerEvents(new TunnelingEvents(), this);
 		loadConfig();
 		//loadGraves();
 
@@ -41,36 +34,11 @@ public class Gauntlet extends JavaPlugin {
 	}
 
 	private void loadGraves() {
-		File inventories = new File(System.getProperty("user.dir").concat("/resources/inventories/graves/"));
-		if (inventories.isDirectory()) {
-			Arrays.stream(inventories.listFiles()).forEach(grave -> {
-				try {
-					GraveEvents.GRAVES.put(grave.getName().substring(0, grave.getName().length() - 5), object_mapper.readValue(grave, Inventory.class));
-				} catch (IOException e) {
-					getServer().getConsoleSender().sendMessage(ChatColor.RED + "Unable to load grave from " + grave.getName() + "...\n" + e);
-				}
-			});
-		}
+
 	}
 
 	private void saveGraves() {
-		GraveEvents.GRAVES.forEach((key, inventory) -> {
-			try {
-				object_mapper.writeValue(new File(System.getProperty("user.dir").concat("/resources/inventories/graves/" + key + ".json")), inventory);
-			} catch (FileNotFoundException e) {
-				File grave = new File(System.getProperty("user.dir").concat("/resources/inventories/graves/" + key + ".json"));
-				grave.getParentFile().mkdirs();
-				try {
-					grave.createNewFile();
-					object_mapper.writeValue(grave, inventory);
-					getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Successfully saved grave for " + key + "!");
-				} catch (IOException e2) {
-					getServer().getConsoleSender().sendMessage(ChatColor.RED + "Unable to save grave for " + key + "...\n" + e2);
-				}
-			} catch (IOException e) {
-				getServer().getConsoleSender().sendMessage(ChatColor.RED + "Unable to save grave for " + key + "...\n" + e);
-			}
-		});
+
 	}
 
 }
