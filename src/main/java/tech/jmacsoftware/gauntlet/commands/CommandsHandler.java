420f3224a3ca1fac9c82634d7b614ed76409f8d0
jaymac00 <jmm0191@auburn.edu>
2024-03-01 13:09:15 -0600

    remove reloadtablists command for now

--- src/main/java/tech/jmacsoftware/gauntlet/commands/CommandsHandler.java
+++ src/main/java/tech/jmacsoftware/gauntlet/commands/CommandsHandler.java
@@ -18,7 +18,7 @@ public class CommandsHandler implements CommandExecutor {
 
 			switch (Commands.resolveByCommand(label)) {
 				case INVALID_COMMAND -> sender.sendMessage(ChatColor.RED + "Command \"" + label + "\" unknown...");
-				case RELOAD_TAB_LISTS -> player.getKiller();
+				case RELOAD_TAB_LISTS -> sender.sendMessage(ChatColor.YELLOW + "Command \"" + label + "\" not implemented yet...");
 				case SET_CUSTOM_MODEL_DATA -> CustomModelDataCommands.setCustomModelData(player, args);
 			}
 		}
