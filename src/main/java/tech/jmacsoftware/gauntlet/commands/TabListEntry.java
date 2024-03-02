5008904cc9aebf522287357d27eabd3bba93074f
jaymac00 <jmm0191@auburn.edu>
2024-03-02 11:07:43 -0600

    reimplement user setmodel command

--- src/main/java/tech/jmacsoftware/gauntlet/commands/TabListEntry.java
+++ src/main/java/tech/jmacsoftware/gauntlet/commands/TabListEntry.java
@@ -1,12 +1,20 @@
 package tech.jmacsoftware.gauntlet.commands;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 public class TabListEntry {
 
 	private String keyword;
 
-	private int value;
+	@Nullable
+	private Integer intValue;
+
+	@Nullable
+	private Double doubleValue;
+
+	@Nullable
+	private String stringValue;
 
 	private List<String> owners;
 
@@ -18,19 +26,35 @@ public class TabListEntry {
 		this.keyword = keyword;
 	}
 
-	public int getValue() {
-		return value;
-	}
-
-	public void setValue(int value) {
-		this.value = value;
-	}
-
-	public List<String> getOwners() {
-		return owners;
+	public int getIntValue() {
+		return intValue;
 	}
 
 	public void setOwners(List<String> owners) {
 		this.owners = owners;
+	}
+
+	public double getDoubleValue() {
+		return doubleValue;
+	}
+
+	public void setDoubleValue(double doubleValue) {
+		this.doubleValue = doubleValue;
+	}
+
+	public String getStringValue() {
+		return stringValue;
+	}
+
+	public void setStringValue(String stringValue) {
+		this.stringValue = stringValue;
+	}
+
+	public void setIntValue(int intValue) {
+		this.intValue = intValue;
+	}
+
+	public List<String> getOwners() {
+		return owners;
 	}
 }
